1e274702279867a4fcb970ddc0cde222b76d9e3e
Timothy H. Keitt <tkeitt@utexas.edu>
2024-12-14 03:07:18 -0600

    fixed order of dimensions

--- R/fft.R
+++ R/fft.R
@@ -1,23 +1,43 @@
 #' Compute the number of samples in an input
 #'
 #' This helper function determines the number of samples in the input object.
+#' For a vector, it returns its length. For a matrix or data frame, it returns
+#' the number of rows.
 #'
-#' @param x An input object (scalar, vector, or matrix).
-#' @return The number of samples (rows) in the input object.
+#' @param x An input object (scalar, vector, matrix, or data frame).
+#' @return An integer representing the number of samples (rows) in the input object.
 #' @keywords internal
 .num_samples <- function(x) {
   input_len <- length(unlist(x))
-  if (is.null(x) || input_len == 0)
-    stop("Input must not be NULL or empty")
+  if (input_len == 0)
+    stop("Input must not be empty")
   floor(ifelse(input_len == 1, x, NROW(x)))
+}
+
+#' Compute Fourier frequencies for default inputs
+#'
+#' Computes normalized Fourier frequencies for scalar or vector inputs, which
+#' are evenly spaced between -0.5 and 0.5.
+#'
+#' @param x A scalar or vector representing the length of the sequence.
+#' @return A numeric vector containing the normalized Fourier frequencies.
+#' @keywords internal
+.fourier_frequencies <- function(x) {
+  n <- .num_samples(x)
+  if (n < 2)
+    stop("Minimum length is 2")
+  k <- 0:(n - 1)
+  ifelse(k <= n / 2, k, k - n) / n
 }
 
 #' Compute Fourier frequencies
 #'
-#' This is a generic function for computing Fourier frequencies.
+#' This is a generic function for computing Fourier frequencies for various
+#' types of inputs, such as scalars, vectors, matrices, time series, or arrays.
 #'
-#' @param x The input (scalar, vector, or time series object).
-#' @return A vector of Fourier frequencies, normalized or scaled based on the input type.
+#' @param x The input object (scalar, vector, matrix, time series, or array).
+#' @return A numeric vector for 1D inputs or a tibble for multidimensional inputs,
+#'         containing the Fourier frequencies normalized or scaled based on the input type.
 #' @export
 fourier_frequencies <- function(x) {
   UseMethod("fourier_frequencies")
@@ -25,42 +45,73 @@ fourier_frequencies <- function(x) {
 
 #' Compute Fourier frequencies for default inputs
 #'
-#' Computes normalized Fourier frequencies for scalar or vector inputs.
+#' Computes normalized Fourier frequencies for scalar or vector inputs, which
+#' represent evenly spaced values in the Fourier domain.
 #'
 #' @param x A scalar or vector representing the length of the sequence.
-#' @return A numeric vector of normalized Fourier frequencies.
+#' @return A tibble with a single column `dim_1` containing the normalized Fourier frequencies.
 #' @examples
 #' fourier_frequencies(8)
 #' @export
 fourier_frequencies.default <- function(x) {
-  n <- .num_samples(x)
-  if (n < 2)
-    stop("Minimum length is 2")
-  k <- 0:(n - 1)
-  return(ifelse(k <= n / 2, k, k - n) / n)
+  tibble::tibble(dim_1 = .fourier_frequencies(x))
 }
 
 #' Compute Fourier frequencies for time series objects
 #'
-#' Computes Fourier frequencies scaled by the frequency attribute of a `ts` object.
+#' Computes Fourier frequencies scaled by the frequency attribute of a `ts` object,
+#' making them domain-specific to the time series.
 #'
 #' @param x A time series (`ts`) object.
-#' @return A numeric vector of scaled Fourier frequencies.
+#' @return A tibble with a single column `dim_1` containing the normalized Fourier frequencies.
 #' @examples
 #' ts_obj <- ts(rnorm(12), frequency = 12)
 #' fourier_frequencies(ts_obj)
 #' @export
 fourier_frequencies.ts <- function(x) {
   n <- length(x)
-  freq <- attr(x, "tsp")[3]
-  return(fourier_frequencies(n) * freq)
+  stride <- attr(x, "tsp")[3]
+  fourier_frequencies(n) |>
+           dplyr::mutate(dim_1 = stride * dim_1)
+}
+
+#' Compute Fourier frequencies for multidimensional arrays
+#'
+#' Computes Fourier frequencies for each dimension of an array and returns
+#' a tidy tibble with frequencies for all dimensions.
+#'
+#' @param x A numeric array.
+#' @return A tibble where each column corresponds to the Fourier frequencies
+#'         of a dimension of the input array. The column names are `dim_1`,
+#'         `dim_2`, ..., corresponding to each dimension.
+#' @examples
+#' # Compute Fourier frequencies for a 3D array
+#' array_input <- array(1:27, dim = c(3, 3, 3))
+#' fourier_frequencies.array(array_input)
+#'
+#' # Compute Fourier frequencies for a 2D matrix
+#' matrix_input <- matrix(1:9, nrow = 3, ncol = 3)
+#' fourier_frequencies.array(matrix_input)
+#'
+#' # Compute Fourier frequencies for a vector (1D case)
+#' vector_input <- 1:8
+#' fourier_frequencies.array(vector_input)
+#' @seealso [fourier_frequencies.default()], [tidyr::expand_grid()]
+#' @export
+fourier_frequencies.array <- function(x) {
+  ff <- list()
+  dims <- dim(drop(x))
+  for (i in rev(seq_along(dims))) {
+    ff[[paste0("dim_", i)]] <- .fourier_frequencies(dims[i])
+  }
+  tidyr::expand_grid(!!!ff)
 }
 
 #' Perform FFT and return a tidy result
 #'
 #' This is a generic function to compute the FFT and return the result in a tidy format.
 #'
-#' @param x Input object (vector or `ts` object).
+#' @param x Input object (vector, time series, or array).
 #' @param repr The desired representation of the FFT result: `"polr"`, `"rect"`, or `"cplx"`.
 #' @return A tibble containing Fourier frequencies and FFT values in the specified format.
 #' @export
@@ -81,25 +132,15 @@ tidy_fft <- function(x, repr = c("polr", "rect", "cplx")) {
 tidy_fft.default <- function(x, repr = c("polr", "rect", "cplx")) {
   repr <- match.arg(repr)
   if (!is.vector(x))
-    stop('Only vector input is supported at this time.')
-  res <- tibble::tibble(frequency = fourier_frequencies(x))
-  fx <- stats::fft(x, inverse = FALSE)
-  if (repr == "cplx")
-    res$fx <- fx
-  if (repr == "rect") {
-    res$re <- Re(fx)
-    res$im <- Im(fx)
-  }
-  if (repr == "polr") {
-    res$mod <- Mod(fx)
-    res$arg <- Arg(fx)
-  }
-  return(structure(
-    res,
-    class = c("tidy_fft", class(res)),
-    is_complex = is.complex(x),
-    repr = repr
-  ))
+    stop('Input must be a vector.')
+  fourier_frequencies(x) |>
+    tibble::add_column(fx = stats::fft(x)) |>
+    structure(
+      class = c("tidy_fft", "tbl_df", "tbl", "data.frame"),
+      is_complex = is.complex(x),
+      repr = "cplx"
+    ) |>
+    change_repr(repr = match.arg(repr))
 }
 
 #' Perform FFT for time series inputs
@@ -114,10 +155,33 @@ tidy_fft.default <- function(x, repr = c("polr", "rect", "cplx")) {
 #' tidy_fft(ts_obj, repr = "polr")
 #' @export
 tidy_fft.ts <- function(x, repr = c("polr", "rect", "cplx")) {
-  res <- tidy_fft(as.vector(x), repr)
-  res$frequency <- fourier_frequencies(x)
-  attr(res, "tsp_orig") <- attr(x, "tsp")
-  return(res)
+  stride = attr(x, "tsp")[3]
+  tidy_fft(as.vector(x), repr = match.arg(repr)) |>
+    dplyr::mutate(dim_1 = .fourier_frequencies(x) * stride) |>
+    structure(tsp_orig = attr(x, "tsp"))
+}
+
+#' Perform FFT for array inputs
+#'
+#' Computes the FFT for an array and returns a tidy result.
+#'
+#' @param x A numeric array.
+#' @param repr The desired representation of the FFT result.
+#' @return A tibble containing Fourier frequencies and FFT values in the specified format.
+#' @examples
+#' arr <- array(1:8, dim = c(2, 2, 2))
+#' tidy_fft(arr, repr = "rect")
+#' @export
+tidy_fft.array <- function(x, repr = c("polr", "rect", "cplx")) {
+  fourier_frequencies(x) |>
+    dplyr::mutate(fx = as.vector(stats::fft(x))) |>
+    structure(
+      class = c("tidy_fft", "tbl_df", "tbl", "data.frame"),
+      is_complex = is.complex(x),
+      dim_orig = dim(x),
+      repr = "cplx"
+    ) |>
+    change_repr(repr = match.arg(repr))
 }
 
 #' Perform inverse FFT on a tidy result
@@ -142,15 +206,20 @@ tidy_ifft <- function(x) {
 #' tidy_ifft(fft_res)
 #' @export
 tidy_ifft.tidy_fft <- function(x) {
-  x <- change_repr(x, "cplx")
-  res <- stats::fft(x$fx, inverse = TRUE) / nrow(x)
+  fx <- change_repr(x, "cplx")$fx
+  res <- if (!is.null(attr(x, "dim_orig"))) {
+    dim(fx) <- attr(x, "dim_orig")
+    stats::fft(fx, inverse = TRUE) / prod(dim(fx))
+  } else {
+    stats::fft(fx, inverse = TRUE) / length(fx)
+  }
   if (attr(x, "is_complex") == FALSE)
     res <- Re(res)
-  if (!is.null(attr(x, "tsp_orig"))) {
+  if (!is.null(attr(x, "tsp_o$ig"))) {
     attr(res, "tsp") <- attr(x, "tsp_orig")
     class(res) <- "ts"
   }
-  return(res)
+  res
 }
 
 #' Retrieve the current representation of a tidy_fft object
@@ -158,32 +227,8 @@ tidy_ifft.tidy_fft <- function(x) {
 #' @param x A `tidy_fft` object.
 #' @return The current representation (`"polr"`, `"rect"`, or `"cplx"`).
 #' @export
-repr <- function(x) {
-  return(attr(x, "repr"))
-}
-
-# Internal function to convert between FFT representations
-# This function is used by `change_repr`.
-.convert_fft <- function(x, from, to) {
-  with(x, {
-    if (from == "cplx" && to == "rect")
-      return(tibble::tibble(re = Re(fx), im = Im(fx)))
-    if (from == "cplx" && to == "polr")
-      return(tibble::tibble(mod = Mod(fx), arg = Arg(fx)))
-    if (from == "rect" && to == "cplx")
-      return(tibble::tibble(fx = complex(
-        real = re, imaginary = im
-      )))
-    if (from == "rect" && to == "polr")
-      return(tibble::tibble(mod = sqrt(re ^ 2 + im ^ 2), arg = atan2(im, re)))
-    if (from == "polr" && to == "cplx")
-      return(tibble::tibble(fx = complex(
-        modulus = mod, argument = arg
-      )))
-    if (from == "polr" && to == "rect")
-      return(tibble::tibble(re = mod * cos(arg), im = mod * sin(arg)))
-    stop("Unsupported conversion.")
-  })
+get_repr <- function(x) {
+  attr(x, "repr")
 }
 
 #' Change the representation of FFT results
@@ -200,19 +245,71 @@ repr <- function(x) {
 change_repr <- function(x, repr = c("polr", "rect", "cplx")) {
   if (!inherits(x, "tidy_fft"))
     stop("Input must be of class 'tidy_fft'")
-  repr <- match.arg(repr)
-  from <- attr(x, "repr")
-  if (from == repr)
+
+  to <- match.arg(repr)
+  from <- get_repr(x)
+
+  if (from == to) {
     return(x)
-  res <- .convert_fft(x, from, repr)
-  res$frequency <- x$frequency
-  return(structure(
-    res,
-    class = class(x),
-    is_complex = attr(x, "is_complex"),
-    tsp_orig = attr(x, "tsp_orig"),
-    repr = repr
-  ))
+  }
+
+  if (from == "cplx" && to == "rect") {
+    return(dplyr::mutate(
+      structure(x, repr = to),
+      re = Re(fx),
+      im = Im(fx),
+      .keep = "unused"
+    ))
+  }
+
+  if (from == "cplx" && to == "polr") {
+    return(dplyr::mutate(
+      structure(x, repr = to),
+      mod = Mod(fx),
+      arg = Arg(fx),
+      .keep = "unused"
+    ))
+  }
+
+  if (from == "rect" && to == "cplx") {
+    return(
+      dplyr::mutate(structure(x, repr = to),
+      fx = complex(real = re, imaginary = im),
+      .keep = "unused")
+    )
+  }
+
+  if (from == "rect" && to == "polr") {
+    return(dplyr::mutate(
+      structure(x, repr = to),
+      mod = Mod(complex(
+        real = re, imaginary = im
+      )),
+      arg = Arg(complex(
+        real = re, imaginary = im
+      )),
+      .keep = "unused"
+    ))
+  }
+
+  if (from == "polr" && to == "cplx") {
+    return(dplyr::mutate(
+      structure(x, repr = to),
+      fx = complex(arg = arg, mod = mod),
+      .keep = "unused"
+    ))
+  }
+
+  if (from == "polr" && to == "rect") {
+    return(dplyr::mutate(
+      structure(x, repr = to),
+      re = Re(complex(arg = arg, mod = mod)),
+      im = Im(complex(arg = arg, mod = mod)),
+      .keep = "unused"
+    ))
+  }
+
+  stop("Cannot convert to", to)
 }
 
 # Declare .data as a global variable to avoid R CMD check NOTE
@@ -228,7 +325,7 @@ utils::globalVariables(c(".data"))
 plot.tidy_fft <- function(x, ...) {
   change_repr(x, "polr") |>
     ggplot2::ggplot(...) +
-    ggplot2::aes(x = .data$frequency, y = .data$mod) +
+    ggplot2::aes(x = .data$dim_1, y = .data$mod) +
     ggplot2::geom_line() +
     ggplot2::ylab("modulus") +
     ggplot2::theme_classic()
--- tests/testthat/test-fft.R
+++ tests/testthat/test-fft.R
@@ -1,50 +1,103 @@
-test_that("fourier_frequencies.default computes correctly", {
-  # Basic case
-  expect_equal(fourier_frequencies.default(4), c(0, 0.25, 0.5, -0.25))
+library(testthat)
 
-  # Larger input
-  expect_equal(
-    fourier_frequencies.default(6),
-    c(0, 1/6, 2/6, 3/6, -2/6, -1/6)
-  )
+test_that(".num_samples computes correctly", {
+  # Vector input
+  expect_equal(.num_samples(c(1, 2, 3)), 3)
 
-  # Invalid input
-  expect_error(fourier_frequencies.default(1), "Minimum length is 2")
+  # Matrix input
+  mat <- matrix(1:9, nrow = 3)
+  expect_equal(.num_samples(mat), 3)
+
+  # Empty input
+  expect_error(.num_samples(numeric(0)), "Input must not be empty")
 })
 
-test_that("fourier_frequencies.ts scales correctly with time series frequency", {
+test_that(".fourier_frequencies computes normalized frequencies", {
+  # Vector input
+  expect_equal(.fourier_frequencies(4), c(0, 0.25, 0.5, -0.25))
+
+  # Larger vector input
+  expect_equal(.fourier_frequencies(6), c(0, 1 / 6, 2 / 6, 3 / 6, -2 / 6, -1 / 6))
+
+  # Invalid input
+  expect_error(.fourier_frequencies(1), "Minimum length is 2")
+})
+
+test_that("fourier_frequencies.default computes correctly", {
+  # Vector input
+  res <- fourier_frequencies.default(4)
+  expect_equal(res$dim_1, c(0, 0.25, 0.5, -0.25))
+
+  # Larger vector input
+  res <- fourier_frequencies.default(6)
+  expect_equal(res$dim_1, c(0, 1 / 6, 2 / 6, 3 / 6, -2 / 6, -1 / 6))
+})
+
+test_that("fourier_frequencies.ts scales frequencies correctly", {
   ts_obj <- ts(c(1, 2, 3, 4, 5, 6), start = c(2000, 1), frequency = 12)
 
-  # Check correct scaling
+  # Check scaled frequencies
   freqs <- fourier_frequencies.ts(ts_obj)
-  expect_equal(freqs, fourier_frequencies.default(6) * 12)
+  expect_equal(freqs, fourier_frequencies(6) |> dplyr::mutate(dim_1 = 12 * dim_1))
 
   # Edge case: Single-point time series
   ts_invalid <- ts(1, start = c(2000, 1), frequency = 12)
   expect_error(fourier_frequencies.ts(ts_invalid), "Minimum length is 2")
 })
 
-test_that("tidy_fft.default produces correct structure", {
+test_that("fourier_frequencies.array computes frequencies for arrays", {
+  # 2D matrix input
+  mat <- matrix(1:9, nrow = 3, ncol = 3)
+  res <- fourier_frequencies(mat)
+  ex <- tidyr::expand_grid(dim_2 = c(0, 1 / 3, -1 / 3),
+                           dim_1 = c(0, 1 / 3, -1 / 3))
+  expect_equal(res, ex)
+
+  # 3D array input
+  arr <- array(1:27, dim = c(3, 3, 3))
+  res <- fourier_frequencies(arr)
+  ex <- tidyr::expand_grid(dim_3 = c(0, 1 / 3, -1 / 3),
+                           dim_2 = c(0, 1 / 3, -1 / 3),
+                           dim_1 = c(0, 1 / 3, -1 / 3))
+  expect_equal(res, ex)
+})
+
+test_that("tidy_fft.default computes FFT results", {
   x <- c(1, 0, -1, 0)
 
-  # Test "cplx" representation
-  res <- tidy_fft.default(x, repr = "cplx")
+  # Complex representation
+  res <- tidy_fft(x, repr = "cplx")
   expect_true(inherits(res, "tidy_fft"))
-  expect_equal(res$frequency, fourier_frequencies.default(x))
+  expect_equal(res$dim_1, fourier_frequencies(x)$dim_1)
   expect_equal(res$fx, stats::fft(x))
 
-  # Test "rect" representation
+  # Rectangular representation
   res_rect <- tidy_fft.default(x, repr = "rect")
   expect_equal(res_rect$re, Re(stats::fft(x)))
   expect_equal(res_rect$im, Im(stats::fft(x)))
 
-  # Test "polr" representation
+  # Polar representation
   res_polr <- tidy_fft.default(x, repr = "polr")
   expect_equal(res_polr$mod, Mod(stats::fft(x)))
   expect_equal(res_polr$arg, Arg(stats::fft(x)))
 })
 
-test_that("tidy_ifft correctly reconstructs the original signal", {
+test_that("tidy_fft.matrix computes 2D FFT results", {
+  mat <- matrix(c(1, 0, -1, 0, 1, 2, 3, 4), nrow = 2)
+
+  # Rectangular representation
+  res <- tidy_fft(mat, repr = "rect")
+  expect_equal(ncol(res), 4)  # dim_1, dim_2, re, im
+  expect_true("dim_1" %in% colnames(res))
+  expect_true("dim_2" %in% colnames(res))
+
+  # Check values
+  fft_res <- stats::fft(mat)
+  expect_equal(res$re, Re(as.vector(fft_res)))
+  expect_equal(res$im, Im(as.vector(fft_res)))
+})
+
+test_that("tidy_ifft reconstructs original signal", {
   x <- c(1, 0, -1, 0)
   fft_res <- tidy_fft.default(x, repr = "cplx")
 
@@ -52,27 +105,77 @@ test_that("tidy_ifft correctly reconstructs the original signal", {
   recon <- tidy_ifft(fft_res)
   expect_equal(recon, x)
 
-  # Check real-only case
-  expect_true(is.numeric(recon))
-
-  # Test with complex input
-  complex_input <- tidy_fft.default(complex(real = x, imaginary = x), repr = "cplx")
-  recon_complex <- tidy_ifft(complex_input)
-  expect_true(is.complex(recon_complex))
+  # Test with matrix
+  mat <- matrix(c(1, 0, -1, 0, 1, 2, 3, 4), nrow = 2)
+  fft_mat_res <- tidy_fft.array(mat, repr = "cplx")
+  recon_mat <- tidy_ifft(fft_mat_res)
+  expect_equal(recon_mat, mat)
 })
 
-test_that("change_repr correctly switches representations", {
-  x <- c(1, 0, -1, 0)
-  fft_res <- tidy_fft.default(x, repr = "cplx")
+test_that("fft(x) matches fourier frequencies for arrays of varying sizes", {
+  # Test with various dimensions
+  dims_list <- list(
+    c(2, 3),       # 2D array
+    c(3, 4, 5),    # 3D array
+    c(1, 5, 2, 3)  # 4D array
+  )
 
-  # Switch to rectangular
-  rect <- change_repr(fft_res, "rect")
-  expect_equal(rect$re, Re(fft_res$fx))
-  expect_equal(rect$im, Im(fft_res$fx))
+  for (dims in dims_list) {
+    # Create an array with the specified dimensions
+    arr <- array(1:prod(dims), dim = dims)
 
-  # Switch to polar
-  polr <- change_repr(rect, "polr")
-  expect_equal(polr$mod, Mod(fft_res$fx))
-  expect_equal(polr$arg, Arg(fft_res$fx))
+    # Compute FFT and frequencies
+    fft_res <- as.vector(fft(arr))
+    dim_grid <- fourier_frequencies(arr)
+
+    # Check that the order matches
+    expect_equal(length(fft_res), nrow(dim_grid),
+                 info = paste("Length mismatch for dims", toString(dims)))
+
+    # Verify that the frequencies match expected combinations
+    expected_freqs <- do.call(tidyr::expand_grid, lapply(rev(dims[dims > 1]), .fourier_frequencies))
+    names(expected_freqs) <- names(dim_grid)
+    expect_equal(dim_grid, expected_freqs,
+                 info = paste("Frequency mismatch for dims", toString(dims)))
+  }
 })
 
+test_that("Impulse position matches expected frequency for row sine wave", {
+  dims <- c(32, 40)  # Row-major input dimensions
+  dim_row <- 8       # Frequency of the sine wave along rows
+  t <- seq(0, 2 * pi, length.out = dims[1])  # Time vector for rows
+  x <- matrix(sin(dim_row * t), nrow = dims[1], ncol = dims[2])  # Input sine wave
+
+  # Perform FFT
+  y <- tidy_fft(x, repr = "polr")
+
+  # Find the frequencies grid
+  dim_grid <- fourier_frequencies(x)
+
+  # Find max impulse in the FFT result
+  impulse <- y[which.max(y$mod), ]
+
+  # Assertions
+  expect_equal(impulse$dim_2, 0, tolerance = 1e-6)             # No variation along columns
+  expect_equal(impulse$dim_1, dim_row / dims[1], tolerance = 1e-6)  # Expected frequency along rows
+})
+
+test_that("Impulse position matches expected frequency for column sine wave", {
+  dims <- c(32, 40)  # Matrix dimensions
+  dim_col <- 10      # Frequency of sine wave along columns
+  t <- seq(0, 2 * pi, length.out = dims[2])  # Time vector for columns
+  x <- matrix(sin(dim_col * t), nrow = dims[1], ncol = dims[2], byrow = TRUE)  # Input sine wave
+
+  # Perform FFT
+  y <- tidy_fft(x, repr = "polr")
+
+  # Frequency grid for the FFT
+  dim_grid <- fourier_frequencies(x)
+
+  # Find max impulse
+  impulse <- y[which.max(y$mod), ]
+
+  # Assertions
+  expect_equal(impulse$dim_1, 0, tolerance = 1e-6)             # No variation along rows
+  expect_equal(impulse$dim_2, dim_col / dims[2], tolerance = 1e-6)  # Expected column frequency
+})
