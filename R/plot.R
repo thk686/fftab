b6315c88571fb93834726fd5109814c721cb9724
Timothy Keitt <tkeitt@gmail.com>
2025-01-13 12:31:50 -0600

    cran fixes

--- R/plot.R
+++ R/plot.R
@@ -2,8 +2,10 @@
 #'
 #' Plots the modulus of the FFT results against the frequencies.
 #'
-#' @param x A `fftab` object.
-#' @param ... passed to ggplot.
+#' @param x A `fftab` object. This should contain Fourier-transformed data.
+#' @param ... Additional arguments passed to `ggplot2::ggplot`.
+#' @return A `ggplot` object representing the modulus of FFT results plotted against the frequencies.
+#' The plot shows the modulus (`mod`) on the y-axis and frequency values on the x-axis.
 #' @exportS3Method graphics::plot
 plot.fftab <- function(x, ...) {
   to_polr(x) |>
